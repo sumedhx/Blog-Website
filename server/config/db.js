059c75dcb00b23a6a95a0b4145cf036b907b981e
Z E R O <72144790+sumedhx@users.noreply.github.com>
2025-01-22 00:06:22 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, { // mongodb+srv://admin:pass@cluster0.en36s.mongodb.net/blog?retryWrites=true&w=majority
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
@@ -19,3 +19,4 @@ module.exports = connectDB;
 
 
 mongoose.set('strictQuery', false);
+
